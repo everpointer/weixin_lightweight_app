1f8b33d8a5d440c9333d1e2d2e429062e7b7c3a8
Star Huang <star@beansmile.com>
2014-04-25 20:35:45 +0800

    firt commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeixinLightweightApp::Application.config.secret_key_base = '7ddb661d38b4601f70ca585a08692efbc8ce3b4ec16a74c5a0b0392c09e2b6eada971b94f0c8bea67d6ac4c685e23c9dc01acee7de2c8efc6aade18e9c80c299'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeixinLightweightApp::Application.config.session_store :cookie_store, key: '_weixin_lightweight_app_session'
